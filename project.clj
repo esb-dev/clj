8640fe2dea1ace6b003bd2aff92b1cd9f365a611
br <Burkhardt.Renz@mni.thm.de>
2018-12-19 09:16:05 +0100

    Clojure 1.10

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://homespages.mni.thm.de/~hg11260"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.1.143"]
-                 [org.clojure/core.async "0.3.465"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/spec.alpha "0.2.176"]
+                 [org.clojure/core.async "0.4.490"]
                  [org.clojure/core.logic "0.8.11"]
                  [org.clojure/tools.macro "0.1.5"]
-                 [org.clojure/tools.trace "0.7.9"]
-                 [seesaw "1.4.5" :exclusions [org.clojure/clojure]]
+                 [org.clojure/tools.trace "0.7.10"]
+                 [seesaw "1.5.0" :exclusions [org.clojure/clojure]]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [markdown-clj "1.0.1"]
+                 [markdown-clj "1.0.5"]
                  [dire "0.5.4"]
-                 [quil "2.6.0"]]
+                 [quil "2.8.0"]]
   :aot [clj.webviewer]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
